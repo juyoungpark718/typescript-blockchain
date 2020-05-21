d13b6f0c0de6953ff7c975121f4f892323ee2a64
박주영 <juyoung7018.park@gmail.com>
2020-05-21 16:33:04 +0900

    how to use class in ts

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+
+  constructor(name: string, age: number, gender?: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person: Human = {
-  name: "juyoung",
-  age: 27,
-  gender: "male",
-};
+const jy = new Human("juyoung", 27);
 
 const sayHi = (person: Human): string => {
   const { name, age, gender } = person;
-  return `hello, my name is ${name}, ${age} years old , ${gender}`;
+  return `hello, my name is ${name}, ${age} years old , ${
+    gender ? gender : "nope"
+  }`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(jy));
 
 export {};
